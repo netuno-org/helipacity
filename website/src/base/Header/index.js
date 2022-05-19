a0d73764c39f4c75b4dc743c3e7961b877337f0a
Luismijias <luismijias@gmail.com>
2022-05-19 18:47:22 -0300

    criação do header

--- website/src/base/Header/index.js
+++ website/src/base/Header/index.js
@@ -1,24 +1,20 @@
-import React, { useState } from 'react';
-import { Layout, Menu } from 'antd';
-import { GlobalOutlined } from '@ant-design/icons';
-import Burger from '@animated-burgers/burger-slip';
-import '@animated-burgers/burger-slip/dist/styles.css';
-import classNames from 'classnames';
-import {
-  Route,
-  Link
-} from "react-router-dom";
-import Cluar from '../../common/Cluar';
-import Builder from '../../common/Builder';
-
-import './index.less';
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+import Burger from "@animated-burgers/burger-slip";
+import "@animated-burgers/burger-slip/dist/styles.css";
+import classNames from "classnames";
+import { Route, Link } from "react-router-dom";
+import Cluar from "../../common/Cluar";
+import Builder from "../../common/Builder";
+import "./index.less";
 
 const { Header } = Layout;
 const { SubMenu } = Menu;
 
 function BaseHeader() {
   const [burgerMenu, setBurgerMenu] = useState(false);
-  const [activeMenu, setActiveMenu] = useState('main');
+  const [activeMenu, setActiveMenu] = useState("main");
 
   const handleMenuClick = (selectMenu) => {
     setBurgerMenu(false);
@@ -38,28 +34,51 @@ function BaseHeader() {
     }
     if (language.code !== Cluar.currentLanguage().code) {
       menuLanguages.push(
-        <Menu.Item key={language.code} onClick={() => {
-          Cluar.changeLanguage(language.locale);
-          window.localStorage.setItem('locale', Cluar.currentLanguage().locale);
-          window.location.href = `/${language.locale}/`;
-        }}>{language.description}</Menu.Item>
+        <Menu.Item
+          key={language.code}
+          onClick={() => {
+            Cluar.changeLanguage(language.locale);
+            window.localStorage.setItem(
+              "locale",
+              Cluar.currentLanguage().locale
+            );
+            window.location.href = `/${language.locale}/`;
+          }}
+        >
+          {language.description}
+        </Menu.Item>
       );
     }
 
     const buildMenu = (page, level) => {
       if (page.menu && language.code === Cluar.currentLanguage().code) {
         const key = `${page.link}`;
-        if (Cluar.pages()[language.code].find((p) => p.menu && p.parent === page.link)) {
+        if (
+          Cluar.pages()[language.code].find(
+            (p) => p.menu && p.parent === page.link
+          )
+        ) {
           subMenuKeys.push(key);
           return (
-            <SubMenu key={key} popupClassName={`menu-level-${level + 1}`} title={
-              page.navigable ?
-                <Link to={`/${Cluar.currentLanguage().locale}${page.link}`} onClick={() => handleMenuClick(key)}>
-                  {page.title}
-                </Link>
-              : <a>{page.title}</a>
-            }>
-              { Cluar.pages()[language.code].filter((p) => p.menu && p.parent === page.link).map((p) => buildMenu(p, level + 1))}
+            <SubMenu
+              key={key}
+              popupClassName={`menu-level-${level + 1}`}
+              title={
+                page.navigable ? (
+                  <Link
+                    to={`/${Cluar.currentLanguage().locale}${page.link}`}
+                    onClick={() => handleMenuClick(key)}
+                  >
+                    {page.title}
+                  </Link>
+                ) : (
+                  <a>{page.title}</a>
+                )
+              }
+            >
+              {Cluar.pages()
+                [language.code].filter((p) => p.menu && p.parent === page.link)
+                .map((p) => buildMenu(p, level + 1))}
             </SubMenu>
           );
         } else {
@@ -84,11 +103,16 @@ function BaseHeader() {
           **/
           return (
             <Menu.Item key={key}>
-              { page.navigable ? 
-                <Link to={`/${Cluar.currentLanguage().locale}${page.link}`} onClick={() => handleMenuClick(key)}>
+              {page.navigable ? (
+                <Link
+                  to={`/${Cluar.currentLanguage().locale}${page.link}`}
+                  onClick={() => handleMenuClick(key)}
+                >
                   {page.title}
                 </Link>
-                : <a>{page.title}</a> }
+              ) : (
+                <a>{page.title}</a>
+              )}
             </Menu.Item>
           );
         }
@@ -98,16 +122,23 @@ function BaseHeader() {
 
     const subroutes = [];
     for (const page of Cluar.pages()[language.code]) {
-      if (page.menu && page.parent === "" && language.code === Cluar.currentLanguage().code) {
-        menu.push(
-          buildMenu(page, 0)
-        );
+      if (
+        page.menu &&
+        page.parent === "" &&
+        language.code === Cluar.currentLanguage().code
+      ) {
+        menu.push(buildMenu(page, 0));
       }
       if (page.navigable == false) {
         continue;
       }
       subroutes.push(
-        <Route key={`/${language.locale}${page.link}`} path={`/${language.locale}${page.link}`} exact element={<Builder page={page} />} />
+        <Route
+          key={`/${language.locale}${page.link}`}
+          path={`/${language.locale}${page.link}`}
+          exact
+          element={<Builder page={page} />}
+        />
       );
     }
     routes.push(
@@ -116,48 +147,62 @@ function BaseHeader() {
       </Route>
     );
   }
-  
+
   return (
-    <Header className={classNames({ 'header-burger-open': burgerMenu })}>
+    <Header  className={classNames({ "header-burger-open": burgerMenu })}>
+      <div className="logo">
+        <Link
+          to={`/${Cluar.currentLanguage().locale}/`}
+          onClick={() => handleMenuClick("/")}
+        >
+          <img alt="logo" src="/images/logo.png" />
+        </Link>
+      </div>
+      <img className="headersvg" src="/images/header.svg"></img>
+
       <div className="ant-layout-header__wrapper">
-        <div className="logo">
-          <Link to={`/${Cluar.currentLanguage().locale}/`} onClick={() => handleMenuClick('/')}>
-            <img alt="logo" src="/images/logo.png" />
-          </Link>
-        </div>
-        <div className={
-          classNames({
-            'menu': true
-          })
-        }>
-          <Menu
+        <div
+          className={classNames({
+            menu: true,
+          })}
+        >
+          {/* <Menu
             theme="light"
             mode="horizontal"
             defaultSelectedKeys={[activeMenu]}
-            selectedKeys={[activeMenu]}>
+            selectedKeys={[activeMenu]}
+          >
             {menu}
-          </Menu>
+          </Menu> */}
         </div>
-        <div className={
-          classNames({
-            'menu': true,
-            'menu-burger': true,
-            'menu-burger-open': burgerMenu
-          })
-        }>
+
+        <div
+          className={classNames({
+            menu: true,
+            "menu-burger": true,
+            "menu-burger-open": burgerMenu,
+          })}
+        >
           <Menu
             theme="light"
             mode="inline"
             defaultSelectedKeys={[activeMenu]}
             selectedKeys={[activeMenu]}
-            openKeys={subMenuKeys}>
+            openKeys={subMenuKeys}
+          >
             {menu}
           </Menu>
         </div>
+
         <div className="menu-burger-button">
-          <Burger isOpen={burgerMenu} onClick={() => { setBurgerMenu(!burgerMenu); }} />
+          <MenuOutlined 
+            isOpen={burgerMenu}
+            onClick={() => {
+              setBurgerMenu(!burgerMenu);
+            }}
+          />
         </div>
-        <Menu
+        {/* <Menu
           theme="light"
           className="menu-languages"
           mode={'horizontal'}
@@ -166,7 +211,8 @@ function BaseHeader() {
           <SubMenu key="langs" icon={<GlobalOutlined />} title={Cluar.currentLanguage().code}>
             {menuLanguages}
           </SubMenu>
-        </Menu>
+        </Menu> */}
+        {/* Descomentar códiogo acima para visializar menu de idiomas.*/}
       </div>
     </Header>
   );
