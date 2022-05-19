586bdad4b773ab0166ccfeb5d8cac9b2bfdf25f3
Melina <melveks@gmail.com>
2022-05-19 23:32:02 +0100

    Customização do conteúdo

--- website/src/components/Content/index.js
+++ website/src/components/Content/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Row, Col } from 'antd';
-
+import{FaMicrophone} from 'react-icons/fa'
+import {BsFillChatSquareTextFill} from 'react-icons/bs'
+import {FiHelpCircle}from 'react-icons/fi'
 import Actions from '../Actions';
+
 
 import './index.less';
 
@@ -13,17 +16,30 @@ function Content({section, type, title, content, image, image_title, image_alt, 
   }
   if (type === 'text') {
     layout = (
-      <div className="content__text">
+      <div >
+        
         <div className="text">
-          <h1>{ title }</h1>
-          { title ? <div className="text__title-border"></div> : null }
-          <div dangerouslySetInnerHTML={{__html: content}}></div>
-        </div>
+        <div className ="barra"> </div>
+          <h1>Fácil Acesso</h1>
+          <h3> Botões Informativos</h3>
+          <div >
+            <button className = "button"><FaMicrophone className="icons"/>Texto</button>
+            <button className = "button"><BsFillChatSquareTextFill className="icons"/>Texto</button>
+            <button className = "button"><FiHelpCircle className="icons"/>Texto</button>
+           </div>
+         </div>
+          <div className ="barra2"> </div>
+          <div className="text">
+          <h1>O que é o Helipacity?</h1>
+          <h3>Somos...</h3>
+          
+          </div >
       </div>
     );
   } else if (type === 'image-left') {
     layout = (
       <div className="content__image-left">
+        
         <Row>
           <Col md={8}>
             <div className="image">
@@ -32,11 +48,25 @@ function Content({section, type, title, content, image, image_title, image_alt, 
           </Col>
           <Col md={16}>
             <div className="text">
-              <h1>{ title }</h1>
+              <p className="titulo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              
               <div dangerouslySetInnerHTML={{__html: content}}></div>
+              
             </div>
           </Col>
         </Row>
+        <div className ="barra3"> </div>
+        <div className ="text">
+        <h1>Por decidir</h1>
+        <h3>Texto</h3>
+        </div>
+        <p className = "titulo__sub-titulo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
       </div>
     );
   } else if (type === 'image-right') {
@@ -45,7 +75,7 @@ function Content({section, type, title, content, image, image_title, image_alt, 
         <Row>
           <Col md={16}>
             <div className="text">
-              <h1>{ title }</h1>
+              <h1> { title }</h1>
               <div className="text__title-border"></div>
               <div dangerouslySetInnerHTML={{__html: content}}></div>
             </div>
@@ -90,6 +120,7 @@ function Content({section, type, title, content, image, image_title, image_alt, 
         </div>
       </div>
     );
+  
   } else {
     layout = (
       <div className={`content__${type}`}>
