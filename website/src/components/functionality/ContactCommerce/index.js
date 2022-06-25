aca506202060dcd3bc35ac232c823ced10ca13f3
Luismijias <luismijias@gmail.com>
2022-06-24 21:52:18 -0300

    Inserindo a funconalidade ContactCommerce.

---
+++ website/src/components/functionality/ContactCommerce/index.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { Row, Col, Form, Input, Button, notification } from 'antd';
+import _service from '@netuno/service-client';
+import Cluar from '../../../common/Cluar';
+
+import './index.less';
+
+
+
+function ContactCommerce({}) {
+    return(
+        <Row>
+        <Col span={24}><h1 className='ContactCommerce'>Em Construção.</h1></Col>
+        </Row>
+        )
+}
+
+export default ContactCommerce;
