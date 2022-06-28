2a722a515dbe219296e305f2725ded218de5eab1
Luismijias <luismijias@gmail.com>
2022-06-27 21:05:12 -0300

    Inserindo dados e ajustando responsividade da Tabela.

--- website/src/components/EstablishmentTable/index.js
+++ website/src/components/EstablishmentTable/index.js
@@ -1,44 +1,49 @@
 import _service from "@netuno/service-client";
 import React, { useEffect, useState } from "react";
-import { Table } from "antd";
+import { Table,Tag } from "antd";
 import './index.less'
 
 const columns = [
   {
+    title: "Categoria",
+    dataIndex: "commerce",
+    className: "commerce",
+    key: "commerce",
+    render: (commerce) => {
+        return <>
+        <Tag>{commerce.name}</Tag>
+        </>
+    },
+  },
+  {
     title: "Nome",
     dataIndex: "name",
     key: "name",
+    className:"name"
   },
   {
     title: "Descrição",
     dataIndex: "description",
     key: "description",
+    className: "description",
   },
   {
     title: "Endereço",
     dataIndex: "address",
     key: "address",
-    class:"teste",
+    className:"address",
   },
   {
     title: "Contato",
     dataIndex: "contact",
     key: "contact",
+    className: "contact",
   },
   {
     title: "Rede Social",
     dataIndex: "link",
     key: "link",
-  },
-  {
-    title: "Categorias",
-    dataIndex: "commerce",
-    key: "commerce",
-    render: (commerce) => {
-        return <>
-        {commerce.name}
-        </>
-    },
+    className: "link",
   },
 ];
 
