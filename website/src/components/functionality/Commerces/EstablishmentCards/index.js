68072172392a998706c1e96ff3c6d512a3ec5068
Luismijias <luismijias@gmail.com>
2022-07-02 11:53:24 -0300

    Realocando as pastas CommerceSelecty e EstablishmentCards e dando uma pequena organizada no código.

---
+++ website/src/components/functionality/Commerces/EstablishmentCards/index.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import _service from "@netuno/service-client";
+import { Card, Row, Col } from "antd";
+import { InstagramOutlined } from "@ant-design/icons";
+import "./index.less";
+
+const { Meta } = Card;
+
+function Cards({}) {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    _service({
+      url: "/establishment/list",
+      success: (response) => {
+        setList(response.json);
+      },
+      fail: (e) => {
+        console.log("Service Error", e);
+      },
+    });
+  }, []);
+  return (
+    <Row>
+      {list.map((item) => {
+        return (
+          <Col
+            xs={{ span: 24 }}
+            sm={{ span: 8 }}
+            md={{ span: 8 }}
+            lg={{ span: 6 }}
+            xl={{ span: 6, offset: 1 }}
+          >
+            <Card
+              hoverable
+              style={{
+                width: 240,
+              }}
+              cover={
+                <img
+                  src={`http://192.168.1.102:9000/services/establishment/image?uid=${item.uid}`}
+                />
+              }
+            >
+              <div className="shadow">Shadow</div>
+              <Meta title={item.name} description={item.description} />
+              <p>{item.address}</p>
+              <p>{item.contact}</p>
+              <a href={item.link}>
+                <InstagramOutlined />
+              </a>
+            </Card>
+          </Col>
+        );
+      })}
+    </Row>
+  );
+}
+export default Cards;
--- website/src/components/functionality/Commerces/index.js
+++ website/src/components/functionality/Commerces/index.js
@@ -1,25 +1,22 @@
-import React, { useState } from 'react';
-import { Row, Col, Form, Input, Button, notification } from 'antd';
-import _service from '@netuno/service-client';
-import Cluar from '../../../common/Cluar';
-
-import './index.less';
-import CommerceSelect from '../../CommerceSelecty';
-import EstablishmentTable from '../../EstablishmentTable';
-
-
+import React, { useState } from "react";
+import { Row, Col, Form, Input, Button, notification } from "antd";
+import _service from "@netuno/service-client";
+import Cluar from "../../../common/Cluar";
+import "./index.less";
+import CommerceSelect from "./CommerceSelecty";
+import Cards from "./EstablishmentCards";
 
 function Commerces({}) {
-  
-    return(
-        <Row>
-              {/* <div className='ContactCommerce'> <h1 >Em Breve.</h1></div> */}
-          <CommerceSelect/>  
-          <EstablishmentTable/> 
-       
-  
-        </Row>
-        )
+  return (
+    <Row>
+      <div className="ContactCommerce">
+        {" "}
+        <h1>Em Breve.</h1>
+      </div>
+      <CommerceSelect />
+      <Cards />
+    </Row>
+  );
 }
 
 export default Commerces;
